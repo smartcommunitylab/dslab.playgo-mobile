8871ed464e7476f1a60c96e771f26b6096a1a44b
Matteo Chini <matteo.chini@gmail.com>
2022-03-01 15:35:49 +0100

    alert service for toast, laoding and confirm

--- playgo-mobile/src/app/auth-pages/auth-callback/auth-callback.page.ts
+++ playgo-mobile/src/app/auth-pages/auth-callback/auth-callback.page.ts
@@ -8,6 +8,8 @@ import {
   AuthService,
 } from 'ionic-appauth';
 import { Subscription } from 'rxjs';
+import { AlertService } from 'src/app/core/shared/services/alert.services';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   templateUrl: './auth-callback.page.html',
@@ -18,7 +20,9 @@ export class AuthCallbackPage implements OnInit, OnDestroy {
   constructor(
     private auth: AuthService,
     private navCtrl: NavController,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService,
+    private translateService: TranslateService
   ) {}
 
   ngOnInit() {
@@ -35,6 +39,7 @@ export class AuthCallbackPage implements OnInit, OnDestroy {
   postCallback(action: IAuthAction) {
     console.log(JSON.stringify(action));
     if (action.action === AuthActions.SignInSuccess) {
+      this.alertService.showToast(this.translateService.instant('login.welcome'));
       this.navCtrl.navigateRoot('/pages/tabs/home');
     }
 
