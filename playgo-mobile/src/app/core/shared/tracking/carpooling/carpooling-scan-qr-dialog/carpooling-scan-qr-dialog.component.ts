d55fad68b064174c264ec15a4b1811e0474bab71
Milan Mikula <mino.mikula@gmail.com>
2022-03-04 14:57:52 +0100

    qr scanner component

--- playgo-mobile/src/app/core/shared/tracking/carpooling/carpooling-scan-qr-dialog/carpooling-scan-qr-dialog.component.ts
+++ playgo-mobile/src/app/core/shared/tracking/carpooling/carpooling-scan-qr-dialog/carpooling-scan-qr-dialog.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { INCORRECT_QR_CODE, QR_CODE_PREFIX } from '../carpooling.service';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-carpooling-scan-qr-dialog',
   templateUrl: './carpooling-scan-qr-dialog.component.html',
   styleUrls: ['./carpooling-scan-qr-dialog.component.scss'],
 })
-export class CarpoolingScanQRDialogComponent implements OnInit {
+export class CarpoolingScanQRDialogComponent implements OnInit, OnDestroy {
   public manualId = '';
 
-  constructor(private modalController: ModalController) {}
+  constructor(private modalController: ModalController) {
+    // FIXME: debug
+    (window as any).BarcodeScanner = BarcodeScanner;
+  }
 
   qrCodeScanned(qrCode: string) {
     if (!qrCode || !qrCode.startsWith(QR_CODE_PREFIX)) {
@@ -31,5 +35,38 @@ export class CarpoolingScanQRDialogComponent implements OnInit {
     this.modalController.dismiss({ id });
   }
 
+  async startScan() {
+    try {
+      await BarcodeScanner.checkPermission({ force: true });
+      // make background of WebView transparent
+      this.hideBackground();
+
+      const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+
+      this.showBackground();
+
+      // if the result has content
+      if (result.hasContent) {
+        console.log(result.content); // log the raw scanned content
+        this.qrCodeScanned(result.content);
+      }
+    } finally {
+      this.showBackground();
+    }
+  }
   ngOnInit() {}
+
+  async hideBackground() {
+    await BarcodeScanner.hideBackground(); // make background of WebView transparent
+    document.body.classList.add('qr-scanner');
+  }
+  async showBackground() {
+    await BarcodeScanner.showBackground();
+    document.body.classList.remove('qr-scanner');
+  }
+
+  ngOnDestroy(): void {
+    this.showBackground();
+    BarcodeScanner.stopScan();
+  }
 }
