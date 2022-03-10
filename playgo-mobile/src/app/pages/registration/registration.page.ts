176c9de25f6528c6d7e1d3a6aef689ca6254486d
Matteo Chini <matteo.chini@gmail.com>
2022-03-10 15:42:46 +0100

    form validator and error

--- playgo-mobile/src/app/pages/registration/registration.page.ts
+++ playgo-mobile/src/app/pages/registration/registration.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TerritoryService } from 'src/app/core/territory/territory.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -8,13 +9,39 @@ import { TerritoryService } from 'src/app/core/territory/territory.service';
 })
 export class RegistrationPage implements OnInit {
   territoryList = [];
-  constructor(private territoryService: TerritoryService) {
+  registrationForm: FormGroup;
+  isSubmitted = false;
+
+  constructor(
+    private territoryService: TerritoryService,
+    public formBuilder: FormBuilder) {
     this.territoryService.territories$.subscribe((territories) => {
       this.territoryList = territories;
     });
   }
 
   ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      lastname: ['', [Validators.required, Validators.minLength(2)]],
+      mail: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      nickname: ['', [Validators.required, Validators.minLength(2)]],
+      language: ['', [Validators.required]],
+      territory: ['', [Validators.required]]
+    });
   }
+  //computed errorcontrol
+  get errorControl() {
+    return this.registrationForm.controls;
+  }
+  registrationSubmit() {
+    this.isSubmitted = true;
+    if (!this.registrationForm.valid) {
+      console.log('Please provide all the required values!');
+      return false;
+    } else {
+      console.log(this.registrationForm.value);
+    }
 
+  }
 }
