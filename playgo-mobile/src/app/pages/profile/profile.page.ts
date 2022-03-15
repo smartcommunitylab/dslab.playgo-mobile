1d050ad6a3d44007ce9fd7c32891aa7c33ac1620
Milan Mikula <mino.mikula@gmail.com>
2022-03-15 16:33:46 +0100

    manual sync

--- playgo-mobile/src/app/pages/profile/profile.page.ts
+++ playgo-mobile/src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { codePush } from 'capacitor-codepush';
 
 @Component({
   selector: 'app-profile',
@@ -11,4 +12,27 @@ export class ProfilePage implements OnInit {
   ngOnInit() {
     //todo call profile service
   }
+
+  async sync() {
+    try {
+      const res = await codePush.sync(
+        {
+          onSyncStatusChanged: (...args) => {
+            console.log('onSyncStatusChanged:', args);
+          },
+        },
+        (...args) => {
+          console.log('Progress:', args);
+        }
+      );
+      console.log('codePush.sync()', res);
+      alert(JSON.stringify(res));
+    } catch (e) {
+      console.error('codePush.sync()', e);
+      alert(e);
+      throw e;
+    }
+  }
 }
+
+(window as any).codePush = codePush;
