9eee35ab84d619b685b62232b8cc3c075a2259af
Milan Mikula <mino.mikula@gmail.com>
2022-04-19 15:58:09 +0200

    using local template

--- playgo-mobile/generate-api.sh
+++ playgo-mobile/generate-api.sh
@@ -1,20 +1,34 @@
 #! /bin/sh
 
+# Generate the API documentation.
+# https://github.com/swagger-api/swagger-codegen/tree/3.0.0
+
 # clear generated files
 rm -rf ./src/app/core/api/generated
 
-# generate using custom template
-java -jar ../../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
-      -i https://backenddev.playngo.it/playandgo/v3/api-docs \
-      -l typescript-angular \
-      --template-dir ../../swagger-codegen/templates/typescript-angular/ \
-      -o ./src/app/core/api/generated \
-      -Dapis -Dmodel -DmodelDocs=false
+# generate using custom template. We are overriding just one file. Default template is here: https://github.com/swagger-api/swagger-codegen-generators/tree/master/src/main/resources/handlebars/typescript-angular
+
+java -jar \
+  swagger-codegen-cli-3.0.34.jar \
+  generate \
+  -i https://backenddev.playngo.it/playandgo/v3/api-docs \
+  -l typescript-angular \
+  --template-dir ./src/app/core/api/templates/ \
+  -o ./src/app/core/api/generated \
 
 # clean unwanted files
+
 rm ./src/app/core/api/generated/*.*
 rm ./src/app/core/api/generated/.gitignore
 rm ./src/app/core/api/generated/.npmignore
+rm -rf ./src/app/core/api/generated/.swagger-codegen/
+rm ./src/app/core/api/generated/api/api.ts
+
+# this file can be used if we want to keep some changes in generated files
+rm ./src/app/core/api/generated/.swagger-codegen-ignore
+
+# better folder name
+mv ./src/app/core/api/generated/api ./src/app/core/api/generated/controllers
 
 # formatting
 npm run prettier ./src/app/core/api/generated/ -- --write
