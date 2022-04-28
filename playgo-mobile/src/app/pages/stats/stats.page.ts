3bea0ef9688bb294108aa301a68229682399ad7d
Matteo Chini <matteo.chini@gmail.com>
2022-04-28 11:55:12 +0200

    playing with chartjs

--- playgo-mobile/src/app/pages/stats/stats.page.ts
+++ playgo-mobile/src/app/pages/stats/stats.page.ts
@@ -1,11 +1,125 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  ArcElement, BarController, BarElement, CategoryScale,
+  Chart, DoughnutController, LinearScale, LineController, LineElement, PointElement
+} from 'chart.js';
+
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.page.html',
   styleUrls: ['./stats.page.scss'],
 })
-export class StatsPage implements OnInit {
-  constructor() {}
+export class StatsPage implements OnInit, AfterViewInit {
+  @ViewChild('barCanvas') private barCanvas: ElementRef;
+  @ViewChild('doughnutCanvas') private doughnutCanvas: ElementRef;
+  @ViewChild('lineCanvas') private lineCanvas: ElementRef;
 
-  ngOnInit() {}
+  barChart: any;
+  doughnutChart: any;
+  lineChart: any;
+
+  constructor() { }
+  ngOnInit() { }
+
+  // When we try to call our chart to initialize methods in ngOnInit() it shows an error nativeElement of undefined.
+  // So, we need to call all chart methods in ngAfterViewInit() where @ViewChild and @ViewChildren will be resolved.
+  ngAfterViewInit() {
+    this.barChartMethod();
+    this.doughnutChartMethod();
+    this.lineChartMethod();
+  }
+
+  barChartMethod() {
+    // Now we need to supply a Chart element reference with an
+    //object that defines the type of chart we want to use, and the type of data we want to display.
+    // eslint-disable-next-line max-len
+    Chart.register(LineController, BarController, CategoryScale, LinearScale, BarElement, DoughnutController, ArcElement, PointElement, LineElement);
+
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [200, 50, 30, 15, 20, 34],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      }
+    });
+  }
+
+  doughnutChartMethod() {
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['BJP', 'Congress', 'AAP', 'CPM', 'SP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [50, 29, 15, 10, 7],
+          backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            '#FFCE56',
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#FF6384'
+          ]
+        }]
+      }
+    });
+  }
+
+  lineChartMethod() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'],
+        datasets: [
+          {
+            label: 'Sell per week',
+            fill: false,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 15],
+            spanGaps: false,
+          }
+        ]
+      }
+    });
+  }
 }
