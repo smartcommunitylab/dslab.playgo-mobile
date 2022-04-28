411e7e5a2b113d2126edd241e24c03dbc54a1c72
Matteo Chini <matteo.chini@gmail.com>
2022-04-28 11:33:55 +0200

    stats for mean with random generate progress line

--- playgo-mobile/src/app/core/shared/profile-components/general-statistics-component/single-mean-statistic-component/single-mean-statistic.component.ts
+++ playgo-mobile/src/app/core/shared/profile-components/general-statistics-component/single-mean-statistic-component/single-mean-statistic.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { TransportStats } from 'src/app/core/api/generated/model/transportStats';
 // import { IMeansStat } from '../../../model/general-statistic.model';
+// import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-single-mean-statistic',
@@ -9,11 +10,15 @@ import { TransportStats } from 'src/app/core/api/generated/model/transportStats'
 })
 export class SingleMeanStatisticComponent implements OnInit, OnChanges {
   @Input() meanStat: TransportStats;
-
+  statValue: number;
+  // @ViewChild('barCanvas') private barCanvas: ElementRef;
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.statValue = Math.random();
+  }
   ngOnChanges(): void {
     console.log(this.meanStat);
   }
+
 }
