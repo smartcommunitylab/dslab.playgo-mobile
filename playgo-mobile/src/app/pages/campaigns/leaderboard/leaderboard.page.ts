bf5ed932fd780a539cf78568bd5773af4d3f6e0d
Milan Mikula <mino.mikula@gmail.com>
2022-05-03 16:19:26 +0200

    type select

--- playgo-mobile/src/app/pages/campaigns/leaderboard/leaderboard.page.ts
+++ playgo-mobile/src/app/pages/campaigns/leaderboard/leaderboard.page.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SelectCustomEvent } from '@ionic/angular';
 import { Dictionary } from 'lodash';
 import { keyBy } from 'lodash-es';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import {
   TransportType,
   transportTypeLabels,
   transportTypes,
 } from 'src/app/core/shared/tracking/trip.model';
+import { tapLog } from 'src/app/core/shared/utils';
 
 @Component({
-  selector: 'app-leaderboars',
+  selector: 'app-leaderboard',
   templateUrl: './leaderboard.page.html',
   styleUrls: ['./leaderboard.page.scss'],
 })
 export class LeaderboardPage implements OnInit {
+  allLeaderboardTypes = allLeaderboardTypes;
   allLeaderboardTypesMap = allLeaderboardTypesMap;
-  allLeaderboardTypes = Object.values(allLeaderboardTypesMap);
 
   campaignId$: Observable<string> = this.route.params.pipe(
     map((params) => params.id)
@@ -27,27 +29,35 @@ export class LeaderboardPage implements OnInit {
     map(() => this.allLeaderboardTypes)
   );
 
+  leaderboardTypeChangedSubject = new Subject<SelectCustomEvent<string>>();
+  selectedLeaderboardTypes$ = this.leaderboardTypeChangedSubject.pipe(
+    tapLog('selectedLeaderboardTypes$'),
+    map((event) => allLeaderboardTypesMap[event.detail.value])
+  );
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {}
 }
 
+const allLeaderboardTypes: LeaderboardType[] = [
+  { value: 'co2', api: 'co2', labelKey: 'campaign.filter.co2' },
+  {
+    value: 'game',
+    api: 'game',
+    gameResource: 'GL',
+    labelKey: 'campaign.filter.GL',
+  },
+  ...transportTypes.map((transportType) => ({
+    value: transportType,
+    api: 'transport' as const,
+    transport: transportType,
+    labelKey: transportTypeLabels[transportType],
+  })),
+];
+
 const allLeaderboardTypesMap: Dictionary<LeaderboardType> = keyBy(
-  [
-    { value: 'co2', api: 'co2', labelKey: 'campaign.filter.co2' },
-    {
-      value: 'game',
-      api: 'game',
-      gameResource: 'GL',
-      labelKey: 'campaign.filter.GL',
-    },
-    ...transportTypes.map((transportType) => ({
-      value: transportType,
-      api: 'transport' as const,
-      transport: transportType,
-      labelKey: transportTypeLabels[transportType],
-    })),
-  ],
+  allLeaderboardTypes,
   'value'
 );
 
