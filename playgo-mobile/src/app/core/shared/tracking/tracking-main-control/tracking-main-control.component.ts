1065da352208d4205a17bcc1761e12b950dd4e48
Milan Mikula <mino.mikula@gmail.com>
2022-06-01 15:50:28 +0200

    running outside of zone

--- playgo-mobile/src/app/core/shared/tracking/tracking-main-control/tracking-main-control.component.ts
+++ playgo-mobile/src/app/core/shared/tracking/tracking-main-control/tracking-main-control.component.ts
@@ -1,22 +1,43 @@
-import { Component, NgZone } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DoCheck,
+  ElementRef,
+  NgZone,
+  OnDestroy,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { map as _map, round } from 'lodash-es';
 import { DateTime, Duration } from 'luxon';
-import { EMPTY, Observable, of, timer } from 'rxjs';
-import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
+import { EMPTY, Observable, of, Subject, Subscription, timer } from 'rxjs';
+import {
+  distinctUntilChanged,
+  map,
+  switchMap,
+  takeUntil,
+  takeWhile,
+} from 'rxjs/operators';
 import { tapLog } from '../../utils';
 // import { map } from 'rxjs/operators';
 import { BackgroundTrackingService } from '../background-tracking.service';
-import { TRIP_END } from '../trip.model';
+import { TripPart, TRIP_END } from '../trip.model';
 import { TripService } from '../trip.service';
 
 @Component({
   selector: 'app-tracking-main-control',
   templateUrl: './tracking-main-control.component.html',
   styleUrls: ['./tracking-main-control.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TrackingMainControlComponent {
-  timeInTripPart$: Observable<string> = this.zone.runOutsideAngular(() =>
-    this.tripService.tripPart$.pipe(
+export class TrackingMainControlComponent implements DoCheck, OnDestroy {
+  @ViewChild('timeElapsed')
+  private timeElapsedElement: ElementRef;
+
+  private isDestroyed$ = new Subject<boolean>();
+  private timeInTripPartOutOfZoneSubject = new Subject<TripPart | TRIP_END>();
+  private timeInTripPart$: Observable<string> =
+    this.timeInTripPartOutOfZoneSubject.pipe(
       switchMap((tripPart) => {
         if (!tripPart || tripPart === TRIP_END) {
           return of(null) as Observable<number>;
@@ -39,13 +60,44 @@ export class TrackingMainControlComponent {
           });
       }),
       distinctUntilChanged(),
-      tapLog('string')
-    )
-  );
+      takeUntil(this.isDestroyed$)
+    );
 
   constructor(
-    public tripService: TripService,
     private zone: NgZone,
+    private renderer: Renderer2,
+    public tripService: TripService,
     public backgroundTrackingService: BackgroundTrackingService
-  ) {}
+  ) {
+    this.tripService.tripPart$.subscribe((tripPart) => {
+      this.zone.runOutsideAngular(() => {
+        this.timeInTripPartOutOfZoneSubject.next(tripPart);
+      });
+    });
+
+    this.timeInTripPart$
+      .pipe(takeUntil(this.isDestroyed$))
+      .subscribe((timeString) => {
+        this.renderTime(timeString);
+      });
+  }
+
+  renderTime(timeString: string) {
+    if (this.timeElapsedElement) {
+      this.renderer.setProperty(
+        this.timeElapsedElement.nativeElement,
+        'innerHTML',
+        timeString
+      );
+    }
+  }
+
+  ngDoCheck() {
+    console.log('tick!');
+  }
+
+  ngOnDestroy() {
+    this.isDestroyed$.next(true);
+    this.isDestroyed$.complete();
+  }
 }
