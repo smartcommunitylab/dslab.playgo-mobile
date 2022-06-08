f1d56460147072f5939e265afd6e65ea7d8e1724
Matteo Chini <matteo.chini@gmail.com>
2022-06-08 16:52:31 +0200

    added components for other main types

--- playgo-mobile/src/app/core/shared/campaigns/home-widget-types/home-campaign-company/home-campaign-company.component.ts
+++ playgo-mobile/src/app/core/shared/campaigns/home-widget-types/home-campaign-company/home-campaign-company.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { DateTime } from 'luxon';
+import { Subscription } from 'rxjs';
+import { CampaignPlacing } from 'src/app/core/api/generated/model/campaignPlacing';
+import { Player } from 'src/app/core/api/generated/model/player';
 import { PlayerCampaign } from 'src/app/core/api/generated/model/playerCampaign';
+import { ReportService } from '../../../services/report.service';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-home-campaign-company',
@@ -9,10 +15,41 @@ import { PlayerCampaign } from 'src/app/core/api/generated/model/playerCampaign'
 export class HomeCampaignCompanyComponent implements OnInit, OnDestroy {
   @Input() campaignContainer: PlayerCampaign;
   @Input() header?: boolean = false;
+  subStat: Subscription;
+  profile: Player;
+  reportWeekStat: CampaignPlacing;
+  reportTotalStat: CampaignPlacing;
+  imagePath: string;
+  constructor(private userService: UserService,
+    private reportService: ReportService) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.imagePath = this.campaignContainer.campaign.logo.url ? this.campaignContainer.campaign.logo.url :
+      'data:image/jpg;base64,' + this.campaignContainer.campaign.logo.image;
+    this.subStat = this.userService.userProfile$.subscribe((profile) => {
+      this.profile = profile;
+      this.reportService
+        .getCo2Stats(
+          this.campaignContainer.campaign.campaignId,
+          this.profile.playerId,
+          DateTime.utc().minus({ week: 1 }).toFormat('yyyy-MM-dd'),
+          DateTime.utc().toFormat('yyyy-MM-dd')
+        )
+        .then((stats) => {
+          this.reportWeekStat = stats;
+        });
+      this.reportService
+        .getCo2Stats(
+          this.campaignContainer.campaign.campaignId,
+          this.profile.playerId
+        )
+        .then((stats) => {
+          this.reportTotalStat = stats;
+        });
+    });
+  }
 
-  ngOnInit() { }
-
-  ngOnDestroy() { }
+  ngOnDestroy() {
+    this.subStat.unsubscribe();
+  }
 }
