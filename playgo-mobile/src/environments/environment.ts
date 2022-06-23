7aac1a01990e6057479dfec0e6de958a6ba024b4
Milan Mikula <mino.mikula@gmail.com>
2022-06-23 10:52:05 +0200

    debug aac client id

--- playgo-mobile/src/environments/environment.ts
+++ playgo-mobile/src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
   authConfig: {
     server_host: 'https://aac.platform.smartcommunitylab.it',
     client_id: 'c_5445634c-95d6-4c0e-a1ff-829b951b91b3',
+    // client_id: 'x_5445634c-95d6-4c0e-a1ff-829b951b91b3',
     redirect_url: 'it.dslab.playgo://callback',
     end_session_redirect_url: 'it.dslab.playgo://endsession',
     scopes: 'openid email profile offline_access',
