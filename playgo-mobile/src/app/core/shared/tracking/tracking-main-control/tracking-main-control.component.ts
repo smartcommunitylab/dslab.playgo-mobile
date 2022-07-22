3513499fcd9d1e44fffb16589d0d7961c3689bc8
Milan Mikula <mino.mikula@gmail.com>
2022-07-22 13:59:30 +0200

    better fab icon behavior

--- playgo-mobile/src/app/core/shared/tracking/tracking-main-control/tracking-main-control.component.ts
+++ playgo-mobile/src/app/core/shared/tracking/tracking-main-control/tracking-main-control.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { filter, mapTo } from 'rxjs/operators';
+import { combineLatest, Observable } from 'rxjs';
+import { filter, map, startWith } from 'rxjs/operators';
+import { PageSettingsService } from '../../services/page-settings.service';
+import { tapLog } from '../../utils';
 // import { map } from 'rxjs/operators';
 import { BackgroundTrackingService } from '../background-tracking.service';
 import { transportTypeIcons, TRIP_END } from '../trip.model';
@@ -14,15 +16,22 @@ import { TripService } from '../trip.service';
 export class TrackingMainControlComponent {
   transportTypeIcons = transportTypeIcons;
   public trackingUIActive = false;
+  public showPlayButton$: Observable<boolean> = combineLatest({
+    pageWithPlayFab: this.pageSettingsService.pageSettings$.pipe(
+      map((pageSettings) => pageSettings.showPlayButton)
+    ),
+    isInTrip: this.tripService.isInTrip$.pipe(startWith(false)),
+  }).pipe(map(({ isInTrip, pageWithPlayFab }) => isInTrip || pageWithPlayFab));
 
   private tripStopped$: Observable<boolean> = this.tripService.tripPart$.pipe(
     filter((tripPart) => tripPart === TRIP_END),
-    mapTo(false)
+    map(() => false)
   );
 
   constructor(
     public tripService: TripService,
-    public backgroundTrackingService: BackgroundTrackingService
+    public backgroundTrackingService: BackgroundTrackingService,
+    private pageSettingsService: PageSettingsService
   ) {
     this.tripStopped$.subscribe(() => {
       this.hideMapAndButtons();
