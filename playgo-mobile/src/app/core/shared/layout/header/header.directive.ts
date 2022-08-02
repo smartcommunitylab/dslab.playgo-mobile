0ac4e8a0cb7a2e8a901236c0998ad1c90e2e3968
Milan Mikula <mino.mikula@gmail.com>
2022-08-02 15:09:07 +0200

    header directive

--- playgo-mobile/src/app/core/shared/layout/header/header.directive.ts
+++ playgo-mobile/src/app/core/shared/layout/header/header.directive.ts
@@ -1,6 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit, ViewContainerRef } from '@angular/core';
+import { HeaderContentComponent } from './header-content.component';
 
 @Directive({
   selector: '[appHeader]',
 })
-export class HeaderDirective {}
+export class HeaderDirective implements OnInit {
+  constructor(
+    private element: ElementRef,
+    private viewContainerRef: ViewContainerRef
+  ) {}
+
+  ngOnInit() {
+    const componentRef = this.viewContainerRef.createComponent(
+      HeaderContentComponent
+    );
+
+    // const host = this.element.nativeElement;
+    // host.insertBefore(componentRef.location.nativeElement, host.firstChild);
+  }
+}
