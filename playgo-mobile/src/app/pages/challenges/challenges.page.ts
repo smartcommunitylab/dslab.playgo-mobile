2b1e5abf071b259d37da012433c59d1973d761ac
Matteo Chini <matteo.chini@gmail.com>
2022-08-04 10:54:16 +0200

    moved challenges observable to a service

--- playgo-mobile/src/app/pages/challenges/challenges.page.ts
+++ playgo-mobile/src/app/pages/challenges/challenges.page.ts
@@ -17,6 +17,7 @@ import { ChallengeConceptInfo } from 'src/app/core/api/generated/model/challenge
 import { ChallengesData } from 'src/app/core/api/generated/model/challengesData';
 import { PlayerCampaign } from 'src/app/core/api/generated/model/playerCampaign';
 import { CampaignService } from 'src/app/core/shared/services/campaign.service';
+import { ChallengeService } from 'src/app/core/shared/services/challenge.service';
 import { ErrorService } from 'src/app/core/shared/services/error.service';
 import { trackByProperty } from 'src/app/core/shared/utils';
 
@@ -28,77 +29,16 @@ import { trackByProperty } from 'src/app/core/shared/utils';
 export class ChallengesPage implements OnInit, OnDestroy {
   selectedSegment?: string;
 
-  private campaignsWithChallenges$ = this.campaignService.myCampaigns$.pipe(
-    map((campaigns) =>
-      // TODO: ask if the condition is correct
-      campaigns.filter((campaign) => campaign.campaign.type === 'city')
-    )
-  );
-
-  public allChallenges$: Observable<Challenge[]> = this.campaignsWithChallenges$
-    .pipe(
-      switchMap((campaigns) =>
-        forkJoin(
-          campaigns.map((campaign) =>
-            this.challengeControllerService
-              .getChallengesUsingGET({
-                campaignId: campaign.campaign.campaignId,
-              })
-              .pipe(
-                this.errorService.getErrorHandler(),
-                map((response) =>
-                  this.processResponseForOneCampaign(response, campaign)
-                )
-              )
-          )
-        ).pipe(map((challengesPerCampaign) => flatten(challengesPerCampaign)))
-      )
-    )
-    .pipe(shareReplay(1));
-
-  public activeChallenges$: Observable<Challenge[]> = this.allChallenges$.pipe(
-    map((challenges) =>
-      challenges.filter((challenge) => challenge.challengeType === 'ACTIVE')
-    )
-  );
-  public pastChallenges$: Observable<Challenge[]> = this.allChallenges$.pipe(
-    map((challenges) =>
-      challenges.filter((challenge) => challenge.challengeType === 'OLD')
-    )
-  );
-  public futureChallenges$: Observable<Challenge[]> = this.allChallenges$.pipe(
-    map((challenges) =>
-      challenges.filter(
-        (challenge) =>
-          challenge.challengeType === 'FUTURE' ||
-          challenge.challengeType === 'PROPOSED'
-      )
-    )
-  );
+  public pastChallenges$: Observable<Challenge[]> =
+    this.challengeService.pastChallenges$;
+  public activeChallenges$: Observable<Challenge[]> =
+    this.challengeService.activeChallenges$;
+  public futureChallenges$: Observable<Challenge[]> =
+    this.challengeService.futureChallenges$;
 
   challengeTracking = trackByProperty<Challenge>('challId');
 
-  constructor(
-    private campaignService: CampaignService,
-    private challengeControllerService: ChallengeControllerService,
-    private errorService: ErrorService
-  ) {}
-
-  private processResponseForOneCampaign(
-    response: ChallengeConceptInfo,
-    campaign: PlayerCampaign
-  ): Challenge[] {
-    const challengesOfAllTypesPerOneCampaign = Object.entries(
-      response.challengeData
-    ).flatMap(([challengeType, challenges]) =>
-      challenges.map((challenge) => ({
-        ...challenge,
-        challengeType: challengeType as ChallengeType,
-        campaign: campaign.campaign,
-      }))
-    );
-    return challengesOfAllTypesPerOneCampaign;
-  }
+  constructor(private challengeService: ChallengeService) {}
 
   ngOnInit(): void {
     this.selectedSegment = 'activeChallenges';
