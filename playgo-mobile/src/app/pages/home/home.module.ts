d02ddcb2f9f7afff51e2e9583a44447f61cf15da
Milan Mikula <mino.mikula@gmail.com>
2022-08-25 15:35:31 +0200

    removed unused component

--- playgo-mobile/src/app/pages/home/home.module.ts
+++ playgo-mobile/src/app/pages/home/home.module.ts
@@ -4,15 +4,10 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 import { MyCampaignsWidgetComponent } from './my-campaigns-widget/my-campaigns-widget.component';
-import { HomeTrackingWidgetComponent } from './home-tracking-widget/home-tracking-widget.component';
 import { PlayGoSharedModule } from 'src/app/core/shared/shared.module';
 @NgModule({
   imports: [PlayGoSharedModule, HomePageRoutingModule],
-  declarations: [
-    MyCampaignsWidgetComponent,
-    HomeTrackingWidgetComponent,
-    HomePage,
-  ],
-  exports: [MyCampaignsWidgetComponent, HomeTrackingWidgetComponent],
+  declarations: [MyCampaignsWidgetComponent, HomePage],
+  exports: [MyCampaignsWidgetComponent],
 })
 export class HomePageModule {}
