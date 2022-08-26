536289647e8e2bb6062bfbbdac8baefcd5fbac36
Milan Mikula <mino.mikula@gmail.com>
2022-08-26 17:14:48 +0200

    angularBuild info

--- playgo-mobile/src/app/pages/home/profile/about-modal/about-modal.component.ts
+++ playgo-mobile/src/app/pages/home/profile/about-modal/about-modal.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { AppStatusService } from 'src/app/core/shared/services/app-status.service';
+import * as internalGitInfo from 'src/assets/git-version.json';
+import { environment } from 'src/environments/environment';
+const gitInfo = internalGitInfo as unknown as {
+  raw: string;
+  hash: string;
+};
 
 @Component({
   selector: 'app-about-modal',
@@ -8,6 +14,11 @@ import { AppStatusService } from 'src/app/core/shared/services/app-status.servic
   styleUrls: ['./about-modal.component.scss'],
 })
 export class AboutModalComponent implements OnInit {
+  angularBuildCommit = gitInfo.raw || gitInfo.hash || '-';
+  angularBuildConfiguration = environment.production
+    ? 'Production'
+    : 'Development';
+
   constructor(
     private modalController: ModalController,
     public appStatusService: AppStatusService
