0c0021b4db89d13fff7237871dc8c73108e22f4f
Milan Mikula <mino.mikula@gmail.com>
2022-09-05 12:52:49 +0200

    getting badges only once per week

--- playgo-mobile/src/app/core/shared/services/badge.service.ts
+++ playgo-mobile/src/app/core/shared/services/badge.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, tap, switchMap, shareReplay, firstValueFrom } from 'rxjs';
 import { GameControllerService } from '../../api/generated/controllers/gameController.service';
-import { BadgeConcept } from '../../api/generated/model/badgeConcept';
 import { BadgesData } from '../../api/generated/model/badgesData';
 import { AuthService } from '../../auth/auth.service';
 import { ErrorService } from './error.service';
@@ -10,9 +10,22 @@ import { LocalStorageService } from './local-storage.service';
   providedIn: 'root',
 })
 export class BadgeService {
-  private allBadgesStorage = this.localStorageService.getStorageOf<{
-    [key: string]: BadgesData;
-  }>('allBadges');
+  private allBadgesStorage =
+    this.localStorageService.getStorageOf<AllBadgesData>('allBadges');
+
+  private allBadges$: Observable<AllBadgesData> =
+    this.authService.isReadyForApi$.pipe(
+      switchMap(() => this.allBadgesStorage.getMeta()),
+      switchMap(({ lastUpdated }) =>
+        this.checkIfStoredBadgesAreUpToDate(lastUpdated)
+          ? this.allBadgesStorage.get()
+          : this.gameControllerService.getAllBadgesUsingGET().pipe(
+              tap((badges) => this.allBadgesStorage.set(badges)),
+              this.errorService.getErrorHandler('silent')
+            )
+      ),
+      shareReplay(1)
+    );
 
   constructor(
     private localStorageService: LocalStorageService,
@@ -22,20 +35,22 @@ export class BadgeService {
   ) {}
 
   init() {
-    this.authService.isReadyForApi$.subscribe(() => {
-      //update all badges and store in local storage
-      //update all badges since the last time (one week)
-      this.gameControllerService
-        .getAllBadgesUsingGET()
-        .pipe(this.errorService.getErrorHandler('silent'))
-        .subscribe((badges) => {
-          console.log(badges);
-          this.allBadgesStorage.set(badges);
-        });
-    });
+    // run api call as soon as possible
+    this.allBadges$.subscribe();
   }
-  // FIXME: this is not efficient!
+
+  // call api only once per week
+  checkIfStoredBadgesAreUpToDate(lastUpdated: number): boolean {
+    const oneWeek = 1000 * 60 * 60 * 24 * 7;
+    return lastUpdated + oneWeek > new Date().getTime();
+  }
+
   public async getBadgeByKey(key: string): Promise<BadgesData> {
-    return (await this.allBadgesStorage?.get())[key];
+    const allBadges = await firstValueFrom(this.allBadges$);
+    return allBadges[key];
   }
 }
+
+type AllBadgesData = {
+  [key: string]: BadgesData;
+};
