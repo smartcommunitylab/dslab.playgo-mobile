ec75596a5f4d0a9ad3ac7fa898f5af64f8eb3228
Matteo Chini <matteo.chini@gmail.com>
2022-09-16 10:07:55 +0200

    fixed game status and score

--- playgo-mobile/src/app/core/shared/campaigns/main-campaign-stat/game-status/game-status.component.ts
+++ playgo-mobile/src/app/core/shared/campaigns/main-campaign-stat/game-status/game-status.component.ts
@@ -13,5 +13,21 @@ export class GameStatusComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.status);
+    console.log(
+      (this.status?.score - this.status?.levels[0]?.startLevelScore) /
+        (this.status?.levels[0]?.endLevelScore -
+          this.status?.levels[0]?.startLevelScore)
+    );
+  }
+  getValueScore() {
+    const score =
+      (this.status?.score - this.status?.levels[0]?.startLevelScore) /
+      (this.status?.levels[0]?.endLevelScore -
+        this.status?.levels[0]?.startLevelScore);
+    if (score > 0) {
+      return score;
+    } else {
+      return 100;
+    }
   }
 }
