4d1433b025f437a2104f1892f86c8139c6ffe4f6
Matteo Chini <matteo.chini@gmail.com>
2022-09-20 11:35:59 +0200

    fix challenge link managment

--- playgo-mobile/src/app/pages/challenges/challenge-card/detail-modal/detail.modal.ts
+++ playgo-mobile/src/app/pages/challenges/challenge-card/detail-modal/detail.modal.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Challenge } from '../../challenges.page';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-detail-modal',
   templateUrl: './detail.modal.html',
   styleUrls: ['./detail.modal.scss'],
 })
-export class DetailChallengenModalPage implements OnInit {
+export class DetailChallengenModalPage implements OnInit, AfterViewInit {
   challenge: Challenge;
+  public anchors: any;
 
-  constructor(private modalController: ModalController) {}
+  constructor(
+    private elementRef: ElementRef,
+    private modalController: ModalController
+  ) {}
+  ngAfterViewInit() {
+    //change the behaviour of _blank arrived with editor, adding a new listener and opening a browser
+    this.anchors = this.elementRef.nativeElement.querySelectorAll('a');
+    this.anchors.forEach((anchor: HTMLAnchorElement) => {
+      anchor.addEventListener('click', this.handleAnchorClick);
+    });
+  }
+  public handleAnchorClick = (event: Event) => {
+    // Prevent opening anchors the default way
+    event.preventDefault();
+    const anchor = event.target as HTMLAnchorElement;
+    Browser.open({
+      url: anchor.href,
+      windowName: '_system',
+      presentationStyle: 'popover',
+    });
+  };
   ngOnInit() {}
   close() {
     this.modalController.dismiss(false);
