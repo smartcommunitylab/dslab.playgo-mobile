4922d3e5e93113dd9d43592ce3760d7999ab2ed0
Milan Mikula <mino.mikula@gmail.com>
2022-09-21 11:48:59 +0200

    fixed dictionary check

--- playgo-mobile/src/app/core/shared/globalization/i18n/check-dictio-runtime.ts
+++ playgo-mobile/src/app/core/shared/globalization/i18n/check-dictio-runtime.ts
@@ -3,22 +3,26 @@ import * as itDictionary from '../../../../../assets/i18n/it.json';
 import * as enDictionary from '../../../../../assets/i18n/en.json';
 
 export function assertDictionariesAreEqualInRuntime() {
-  const itKeys = allKeys({ ...itDictionary });
-  const enKeys = allKeys({ ...enDictionary });
+  const itKeys = allKeys(getJsonModuleContent(itDictionary));
+  const enKeys = allKeys(getJsonModuleContent(enDictionary));
 
   const areDictionariesEqual = isEqual(itKeys, enKeys);
 
   if (!areDictionariesEqual) {
     const missingKeysInIt = enKeys.filter((key) => !itKeys.includes(key));
     const missingKeysInEn = itKeys.filter((key) => !enKeys.includes(key));
-    console.error(
-      'Dictionaries are not equal! Missing keys in italian dictionary',
-      missingKeysInIt
-    );
-    console.error(
-      'Dictionaries are not equal! Missing keys in english dictionary',
-      missingKeysInEn
-    );
+    if (missingKeysInIt.length) {
+      console.error(
+        'Dictionaries are not equal! Missing keys in italian dictionary',
+        missingKeysInIt
+      );
+    }
+    if (missingKeysInEn.length) {
+      console.error(
+        'Dictionaries are not equal! Missing keys in english dictionary',
+        missingKeysInEn
+      );
+    }
   }
 }
 
@@ -32,3 +36,11 @@ const allKeys = (o: any, prefix = '', out: string[] = []) => {
   }
   return out;
 };
+
+function getJsonModuleContent<T>(jsonModule: T): T {
+  // eslint-disable-next-line no-underscore-dangle
+  if ((jsonModule as any).__esModule) {
+    return (jsonModule as any).default;
+  }
+  return jsonModule;
+}
