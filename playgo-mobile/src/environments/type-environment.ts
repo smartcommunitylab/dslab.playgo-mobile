d089996fe7c732ae6fbfa423fdd762803648dfef
Milan Mikula <mino.mikula@gmail.com>
2022-09-22 13:27:18 +0200

    explicit dev environment

---
+++ playgo-mobile/src/environments/type-environment.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+export interface Environment {
+  production: boolean;
+  useCodePush: boolean;
+  support: {
+    privacy: string;
+    faq: string;
+    email: string;
+  };
+  authConfig: {
+    server_host: string;
+    client_id: string;
+    redirect_url: string;
+    end_session_redirect_url: string;
+    scopes: string;
+    automaticSilentRenew: boolean;
+    extras: { prompt: string; idp_hint: string };
+    pkce: boolean;
+  };
+  idp_hint: {
+    facebook: string;
+    google: string;
+    apple: string;
+  };
+  serverUrl: {
+    api: string;
+    apiPath: string;
+    pgaziendeUrl: string;
+    hscApi: string;
+  };
+  firebaseConfig: {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+    measurementId: string;
+  };
+}
