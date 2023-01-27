00e76a7eac4bb7aafd76ac8b01612d2f6a7fe100
Matteo Chini <matteo.chini@gmail.com>
2023-01-27 11:09:45 +0100

    fixed percentage for school comp

--- playgo-mobile/src/app/core/shared/campaigns/home-widget-types/home-campaign-school/home-school-progression/home-school-progression.component.ts
+++ playgo-mobile/src/app/core/shared/campaigns/home-widget-types/home-campaign-school/home-school-progression/home-school-progression.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { PlacingComparison } from 'src/app/core/api/generated-hsc/model/placingComparison';
-import { TeamService } from 'src/app/core/shared/services/team.service';
 import { ComparisonModalPage } from './comparison-modal/comparison.modal';
 
 
@@ -29,7 +28,7 @@ export class HomeSchoolProgressionComponent implements OnInit, OnDestroy {
   }
   getPercentage(arg0: PlacingComparison) {
     return (arg0.value - arg0.min) > 0 ?
-      ((arg0.max - arg0.min) / (arg0.value - arg0.min)) :
+      ((arg0.value - arg0.min) / (arg0.max - arg0.min)) :
       (((arg0.max === arg0.min && arg0.min === arg0.value && arg0.value === 0) ||
         (arg0.value === arg0.min && arg0.value !== arg0.max)) ? 0 : 100);
   }
