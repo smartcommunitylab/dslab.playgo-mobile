e1eab446906fec7b7e667a6c3916a74014a289af
Matteo Chini <matteo.chini@gmail.com>
2023-03-01 16:53:29 +0100

    new email support

--- playgo-mobile/src/app/pages/home/footer/footer.component.ts
+++ playgo-mobile/src/app/pages/home/footer/footer.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AppStatusService } from 'src/app/core/shared/services/app-status.service';
 import { Browser } from '@capacitor/browser';
 import { environment } from 'src/environments/environment';
 import { ModalController } from '@ionic/angular';
 import { CreditsModalComponent } from './credits-modal/credits-modal.component';
-import { UserService } from 'src/app/core/shared/services/user.service';
+import { User, UserService } from 'src/app/core/shared/services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
+  subStat: Subscription;
+  profile: User;
+
   constructor(public appStatusService: AppStatusService, private modalController: ModalController, private userService: UserService) { }
   async ngOnInit() {
-
+    this.subStat = this.userService.userProfile$
+      .subscribe((profile) => {
+        this.profile = profile;
+      });
+  }
+  ngOnDestroy() {
+    this.subStat.unsubscribe();
   }
   async openCredits() {
     const modal = await this.modalController.create({
@@ -43,6 +53,9 @@ export class FooterComponent implements OnInit {
     });
   }
   openHelp() {
-    window.open('mailto:' + environment.support.email);
+    window.open('mailto:' + environment.support.email +
+      '?subject=Play%26go%20Supporto&body=-------------------------------------------------%0D%0A%0D%0AterritoryId: ' +
+      this.profile.territoryId + '%0D%0AplayerId: ' +
+      this.profile.playerId + '%0D%0A%0D%0A-------------------------------------------------');
   }
 }
