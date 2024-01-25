09bbccb9284cf98f7466e1490e962333919bf4e2
Matteo Chini <matteo.chini@gmail.com>
2024-01-25 10:27:56 +0100

    manage of limits for company

--- playgo-mobile/src/app/core/shared/campaigns/main-campaign-stat/limit-status/limit-modal/limit.modal.ts
+++ playgo-mobile/src/app/core/shared/campaigns/main-campaign-stat/limit-status/limit-modal/limit.modal.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Campaign } from 'src/app/core/api/generated/model/campaign';
+
 @Component({
   selector: 'app-limit-modal',
   templateUrl: './limit.modal.html',
   styleUrls: ['./limit.modal.scss'],
 })
 export class LimitModalPage implements OnInit {
-
+  campaign: Campaign;
   constructor(private modalController: ModalController) { }
   ngOnInit() { }
   close() {
--- playgo-mobile/src/app/core/shared/campaigns/main-campaign-stat/limit-status/limit-status.component.ts
+++ playgo-mobile/src/app/core/shared/campaigns/main-campaign-stat/limit-status/limit-status.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { LimitModalPage } from './limit-modal/limit.modal';
+import { UserService } from '../../../services/user.service';
+import { Campaign } from 'src/app/core/api/generated/model/campaign';
 
 @Component({
   selector: 'app-limit-status',
@@ -8,21 +10,27 @@ import { LimitModalPage } from './limit-modal/limit.modal';
   styleUrls: ['./limit-status.component.scss'],
 })
 export class LimitStatusComponent implements OnInit {
+  @Input() campaign?: Campaign = undefined;
   @Input() limitMax?: any = undefined;
   @Input() limitValue?: any = undefined;
   @Input() type?: string;
   @Input() header?: string;
   @Input() infoBox?= false;
   @Input() virtualScoreLabel?: string;
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private userService: UserService) { }
 
   ngOnInit() {
     // console.log('limitMax', this.limitMax, 'limitValue', this.limitValue);
   }
   async openLimit(event: any) {
     event.stopPropagation();
+    const language = this.userService.getLanguage();
     const modal = await this.modalController.create({
       component: LimitModalPage,
+      componentProps: {
+        campaign: this.campaign,
+        language,
+      },
       cssClass: 'challenge-info',
       swipeToClose: true,
     });
